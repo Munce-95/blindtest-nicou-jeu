ed032f8dadded447770b30164299679bd9d7fe53
Celian Gremy <celian.gremy@gmail.com>
2026-05-10 20:59:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // On ajoute cette ligne pour que les liens vers les fichiers soient relatifs au nom de ton repo
+  base: '/blindtest-nicou-jeu/', 
 })
